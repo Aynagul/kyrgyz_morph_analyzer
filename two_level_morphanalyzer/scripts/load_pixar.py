ce3d501be3eb713582c565eddc0f5c087728caad
baiyshbaike <baiyshbeksamatov@gmail.com>
2023-05-08 19:30:11 +0600

    added all_noun

--- two_level_morphanalyzer/scripts/load_pixar.py
+++ two_level_morphanalyzer/scripts/load_pixar.py
@@ -2,8 +2,8 @@ from analyzer.models import AllRoot,Tags,PartOfSpeech
 import csv
 
 def run():
-    with open('all.csv', encoding='utf-8') as file:
-        reader = csv.reader(file, delimiter=';')
+    with open('All_noun.csv', encoding='utf-8') as file:
+        reader = csv.reader(file, delimiter=',')
         # AllRoot.objects.all().delete()
         # Tags.objects.all().delete()
         # PartOf.objects.all().delete()
@@ -32,33 +32,6 @@ def run():
             if partof:
                 allroot, created = AllRoot.objects.get_or_create(root=row[0].lower(), part_of_speech=partof)
                 # Add the Tag objects to the AllRoot's tag field
+                print(row[0])
                 if created:
                     allroot.tag.add(*tag_objs)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            # # Split genres by semicolon and remove leading/trailing spaces
-            # genre_names = [genre.strip() for genre in row[1].split(";")]
-            
-            # # Get or create all genres for the current film
-            # genres = [Genre.objects.get_or_create(name=genre_name)[0]
-            #           for genre_name in genre_names]
-            
-            # # Create the film object and add all genres to it
-            # film = Film.objects.create(title=row[0], length=1, year=2)
-            # film.genre.set(genres)
